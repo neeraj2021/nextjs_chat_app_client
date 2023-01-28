8e6cb7201218f74e4effa4dbd68311da12481d60
Neeraj Kumar <neerajkmahawar2017@gmail.com>
2023-01-28 13:06:03 +0530

    dev: add support of prod and local DB

--- axios.js
+++ axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3001/";
+const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
 const axiosInstance = axios.create({
   baseURL,
   withCredentials: true,
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-}
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: process.env.API_BASE_URL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
